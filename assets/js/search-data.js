21a01182e711f9c4cb561b149f2dcf8fef495d0c
adalheimer <81051536+adalheimer@users.noreply.github.com>
2025-09-03 19:03:34 +0000

    Deploying to gh-pages from @ adalheimer/adalheimer.github.io@1df8fa3720ee6eb0ed6d6e9a5f11cce040eddb18 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-teaching",
           title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
+          description: "Taught courses.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/teaching/";
