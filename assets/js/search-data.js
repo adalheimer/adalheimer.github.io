53823d0d0f528c21a6a2ca0bfacc2aeb6e3f5672
adalheimer <81051536+adalheimer@users.noreply.github.com>
2025-09-04 11:46:10 +0000

    Deploying to gh-pages from @ adalheimer/adalheimer.github.io@821a9605d93f134039ded844a309ca66bde69a44 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,14 +30,7 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "dropdown-bookshelf",
-              title: "bookshelf",
-              description: "",
-              section: "Dropdown",
-              handler: () => {
-                window.location.href = "/books/";
-              },
-            },{id: "books-the-godfather",
+        },{id: "books-the-godfather",
           title: 'The Godfather',
           description: "",
           section: "Books",handler: () => {
